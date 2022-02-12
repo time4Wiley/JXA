2f966e47db0f187694b605c60a097fba5da986db
Wei Sun <sunwei415@126.com>
2022-02-12 16:56:57 +0800

    testing Application failed

--- example/src/cli.ts
+++ example/src/cli.ts
@@ -1,6 +1,11 @@
-import { example } from "./example";
-example().then((output) => {
-    console.log(output)
-}).catch(error => {
-    console.error(error);
-});
+// import { example } from "./example";
+import "@jxa/global-type";
+
+const theBrainApp = Application('TheBrain 12');
+theBrainApp.activate();
+
+// example().then((output) => {
+//     console.log(output)
+// }).catch(error => {
+//     console.error(error);
+// });
